9912c7a635758658b2b359883b9d4c1f3a20f231
sammykae <sammykay301@live.com>
2022-11-29 10:35:11 +0100

    js warmup

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+let myArr = ["C is fun", "Python is cool", "Javascript is amazing"];
+for (const line of myArr) {
+	console.log(line);
 }
